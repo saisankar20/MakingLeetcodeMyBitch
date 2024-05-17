f5bdc4dea6615c67243a7c7cd0522cfbf9278e6d
sb23w <159970985+sb23w@users.noreply.github.com>
2024-05-16 19:52:14 -0700

    Create Leetcode217.py
    First Leetcode i solved although did it by following Neetcode

---
+++ Leetcode217.py
@@ -0,0 +1,11 @@
+#https://www.youtube.com/watch?v=3OamzN90kPg
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+        
