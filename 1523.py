87af1bab27c6709e665fa1d0d1383a19a2025b4e
sai sankar bhuvanapalli <40035693+saisankar20@users.noreply.github.com>
2025-01-03 17:36:22 -0500

    Create 1523.py

---
+++ 1523.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        count = 0
+        for i in range(low , high+1):
+            if (i % 2) != 0:
+                count += 1
+        return count
