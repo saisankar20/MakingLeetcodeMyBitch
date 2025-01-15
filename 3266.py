d4c6f9acc9afefd54db067938cee24a7669457d4
sai sankar bhuvanapalli <40035693+saisankar20@users.noreply.github.com>
2025-01-15 17:54:47 -0500

    Update 3266.py

--- 3266.py
+++ 3266.py
@@ -1,13 +1,17 @@
-from typing import List
-
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
-        MOD = 10**9 + 7 
-        
-        for _ in range(k):
-            
-            min_index = nums.index(min(nums))
-            
-            nums[min_index] = (nums[min_index] * multiplier) % MOD
-        
+        if multiplier == 1:
+            return nums
+        pq = [(x, i) for i, x in enumerate(nums)]
+        heapify(pq)
+        m = max(nums)
+        while k and pq[0][0] < m:
+            x, i = heappop(pq)
+            heappush(pq, (x * multiplier, i))
+            k -= 1
+        n = len(nums)
+        mod = 10**9 + 7
+        pq.sort()
+        for i, (x, j) in enumerate(pq):
+            nums[j] = x * pow(multiplier, k // n + int(i < k % n), mod) % mod
         return nums
